0ed9b9e4df940ac6e62e6431ef4b9a91fa2eb8f1
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-07-12 01:46:24 +0530

    Angular modules and controllers 1. Creating Modules, 2. Creating Controller, 3. Register controller with Module.

---
+++ Sample_AngularJS_Application/Scripts/CustomScript.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("MyModule", []); /*Created a module*/
+
+var myController = function ($scope) {    /*Created a controller*/
+    $scope.message = "Angular Tutorial"
+};
+
+myApp.controller("MyController", myController);  /*Register contoller with module*/
