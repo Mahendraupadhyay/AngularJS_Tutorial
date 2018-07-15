accb7f46d84872b4b4e6d7364d6368f9909abda0
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-07-15 21:21:23 +0530

    Module chaining mechanism to create a module, Create the Controller and register controller with module all in single line.

--- Sample_AngularJS_Application/Scripts/CustomScript.js
+++ Sample_AngularJS_Application/Scripts/CustomScript.js
@@ -1,14 +1,14 @@
 ﻿/// <reference path="angular.min.js" />
 
-var myApp = angular.module("MyModule", []); /*Created a module*/
 
-var myController = function ($scope) {  
-    var employee = {
-        fisrtName:"Mahendra",
-        lastName:"Upadhyay",
-        gender:"Male"
-    };
-    $scope.employee = employee;
-};
 
-myApp.controller("MyController", myController);  /*Register contoller with module*/
+var myApp = angular
+    .module("MyModule", [])
+    .controller("MyController", function ($scope) {
+        var employee = {
+            firstName: "Mahendra",
+            lastName: "Upadhyay",
+            gender: "Male"
+        };
+        $scope.employee = employee;
+    });
