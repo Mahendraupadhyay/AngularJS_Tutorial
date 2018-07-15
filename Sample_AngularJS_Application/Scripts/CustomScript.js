5d0814118a53254f81632fee44ef4c9e9c462a1e
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-07-16 01:17:35 +0530

    1. angularjs ng-repeat example 2. ng-repeat simple example 3. ng-repeat simple array 4. angularjs ng repeat nested array 5. angularjs ng repeat index example

--- Sample_AngularJS_Application/Scripts/CustomScript.js
+++ Sample_AngularJS_Application/Scripts/CustomScript.js
@@ -1,13 +1,25 @@
 ﻿/// <reference path="angular.min.js" />
 
-
-
 var myApp = angular
     .module("MyModule", [])
     .controller("MyController", function ($scope) {
-        var country = {
-            name: "India",
-            flag: "/Image/pexels-photo-248797.jpeg"
-        };
-        $scope.country = country;
+        //var employees = [
+        //    { name : "Name1", gender : "M", Salary : 13 },
+        //    { name : "Name2", gender :"F", Salary : 133 },
+        //    { name : "Name3", gender : "O", Salary : 132 }];       
+
+        var countries = [
+            { 
+                name:"India",
+                cities:[{ name:"Mumbai" }, { name:"Bhopal" }, { name:"Delhi" }]
+            },
+            {
+                name:"US",
+                cities:[{ name:"US City1" }, { name:"US City2" }, { name:"US City3" }]
+            },
+            {
+                name:"UK",
+                cities:[{ name:"UK City1" }, { name:"UK City2" }, { name:"UK City3" }]
+            }];
+        $scope.countries = countries;
     });
