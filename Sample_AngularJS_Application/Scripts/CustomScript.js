bf990002cb29d5fef5afda445d3e825b9c0d80ed
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-07-20 00:50:30 +0530

    AngularJS sort rows by table header

--- Sample_AngularJS_Application/Scripts/CustomScript.js
+++ Sample_AngularJS_Application/Scripts/CustomScript.js
@@ -7,13 +7,32 @@ var myApp = angular
         var employees = [
             { name: "Emp1", dob: new Date("November 23,1980"), gender: "Male", Salary: 55224 },
             { name: "Emp2", dob: new Date("08/25/1992"), gender: "Female", Salary: 56482 },
+            { name: "Emp3", dob: new Date("12/16/2018"), gender: "Male", Salary: 12100.023 },
             { name: "Emp4", dob: new Date("12/6/2018"), gender: "Male", Salary: 1200.023 },
-            { name: "Emp5", dob: new Date("22/12/1998"), gender: "Male", Salary: 1548957.2356 },];
+            { name: "Emp5", dob: new Date("2/12/1998"), gender: "Male", Salary: 1548957.2356 }
+        ];
 
         $scope.employees = employees;
-        $scope.rowLimit = 3;
+        $scope.sortColumn = "name";
+        $scope.reverseSort = false;
 
-         //var technologies = [    //Event Handling in Angular
+
+        /*AngularJS sort rows by table header Start*/
+        $scope.sortData = function (column) {
+            $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+            $scope.sortColumn = column;
+        }
+
+        $scope.getSortClass = function (column) {
+            if ($scope.sortColumn == column) {
+                return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+            }
+            return '';
+        }
+    });
+        /*AngularJS sort rows by table header End*/
+
+        //var technologies = [    //Event Handling in Angular
         //    { name: "C#", likes: 0, dislikes: 0 },
         //    { name: "ASP.Net", likes: 0, dislikes: 0 },
         //    { name: "WCF", likes: 0, dislikes: 0 }];
@@ -42,7 +61,5 @@ var myApp = angular
         //    { name : "Name2", gender :"F", Salary : 133 },
         //    { name : "Name3", gender : "O", Salary : 132 }];       
 
-        
 
-       
-    });
+
